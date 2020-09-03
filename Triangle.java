2e7514539be654b3664b681d2580bdd6f1a075f9
KarlSchaller <49100929+KarlSchaller@users.noreply.github.com>
2020-09-03 09:41:26 -0400

    implement triangle

--- Triangle.java
+++ Triangle.java
@@ -11,4 +11,28 @@ package schaller_assignment0;
  */
 public class Triangle extends Shape {
     
+    private double a, b, c;
+    
+    public Triangle(String name) {
+        super(name);
+    }
+    
+    public void setDimensions(double side1, double side2, double side3) {
+        this.a = side1;
+        this.b = side2;
+        this.c = side3;
+    }
+    
+    @Override
+    public void printDimensions(){
+        System.out.println("Side 1: " + a);
+        System.out.println("Side 2: " + b);
+        System.out.println("Side 3: " + c);
+    }
+    
+    @Override
+    public double getArea() {
+        double s = (a + b + c) / 2;
+        return Math.sqrt(s*(s-a)*(s-b)*(s-c));
+    }
 }
