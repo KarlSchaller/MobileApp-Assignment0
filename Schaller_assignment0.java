fa2752dc1dfa15d85c5744075d528fe3cfbc9d68
KarlSchaller <49100929+KarlSchaller@users.noreply.github.com>
2020-09-03 09:19:12 -0400

    add main and shape classes

---
+++ Schaller_assignment0.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package schaller_assignment0;
+
+/**
+ *
+ * @author Karl
+ */
+public class Schaller_assignment0 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
