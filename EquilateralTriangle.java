f088359db6f1653400a84a9939f554ab83968413
KarlSchaller <49100929+KarlSchaller@users.noreply.github.com>
2020-09-03 09:27:14 -0400

    add class heirarchy

---
+++ EquilateralTriangle.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package schaller_assignment0;
+
+/**
+ *
+ * @author Karl
+ */
+public class EquilateralTriangle extends Triangle {
+    
+}
