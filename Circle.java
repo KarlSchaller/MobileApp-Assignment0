3fc6b28ff16be149ea98a4c3d2ae9c8f6b541106
KarlSchaller <49100929+KarlSchaller@users.noreply.github.com>
2020-09-03 09:37:20 -0400

    implement circle

--- Circle.java
+++ Circle.java
@@ -11,4 +11,24 @@ package schaller_assignment0;
  */
 public class Circle extends Shape {
     
+    private double radius;
+    
+    public Circle(String name) {
+        super(name);
+    }
+    
+    public void setDimensions(double radius) {
+        this.radius = radius;
+    }
+    
+    @Override
+    public void printDimensions(){
+        System.out.println("Radius: " + radius);
+    }
+    
+    @Override
+    public double getArea() {
+        return Math.PI*radius*radius;
+    }
+    
 }
