969ae37b4f8295ef64f101c7c91fdb0a249a32e1
KarlSchaller <49100929+KarlSchaller@users.noreply.github.com>
2020-09-03 09:34:42 -0400

    implement square

--- Square.java
+++ Square.java
@@ -11,4 +11,25 @@ package schaller_assignment0;
  */
 public class Square extends Shape {
     
+    private double length, height;
+    
+    public Square(String name) {
+        super(name);
+    }
+    
+    public void setDimensions(double length, double height) {
+        this.length = length;
+        this.height = height;
+    }
+    
+    @Override
+    public void printDimensions(){
+        System.out.println("Length: " + length);
+        System.out.println("Height: " + height);
+    }
+    
+    @Override
+    public double getArea() {
+        return length*height;
+    }
 }
